7f27faa3d08d5c57c440683497c0c219ce9c13bc
Your Name <you@example.com>
2021-02-04 14:02:18 +0300

    contenful options

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,3 +32,27 @@ module.exports = {
     // `gatsby-plugin-offline`,
   ],
 }
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  /* other settings */
+  plugins: [
+    /* other plugins */
+    {
+      resolve: `gatsby-transformer-sharp`
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    }
+  ]
+}
